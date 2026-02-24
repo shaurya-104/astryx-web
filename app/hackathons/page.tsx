1ee9500c30ed906e11c20afc7486a1fc57432241
Shaurya <shauryaadityasinghthakur@gmail.com>
2026-02-24 16:29:13 +0530

    update

--- app/hackathons/page.tsx
+++ app/hackathons/page.tsx
@@ -44,7 +44,7 @@ export default function HackathonsPage() {
 
           <h1 className="ignite-title">IGNITE</h1>
 
-          <p className="ignite-tagline">Ignite the code within you.</p>
+          <p className="ignite-tagline">Ignite the coder within you.</p>
         </div>
       </section>
 
